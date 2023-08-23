8c12a41994b58e516d0ce4c1db213963a68b2ba6
Juuso Hakala <juuso.hakala@myy.haaga-helia.fi>
2023-08-23 12:11:55 +0300

    task done

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Alert,
+  TextInput,
+  Image,
+} from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [text, setText] = useState("");
+  
+  const buttonPressed = () => {
+    console.log("Button pressed");
+  }
+  
+  const showAlert = () => {
+    Alert.alert("Yo!");
+  }
+
+  const showTextState = () => {
+    Alert.alert(text);
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello world!</Text>
+      <Button onPress={buttonPressed} title="Click this" />
+      <Button onPress={showAlert} title="Show alert" />
+      <TextInput
+        style={styles.input}
+        onChangeText={text => setText(text)}
+        value={text}
+      />
+      <Button onPress={showTextState} title="Show text" />
+      <Image
+        style={styles.image}
+        source={{uri: "https://docs.expo.dev/static/images/tutorial/01-app-running-on-all-platforms.jpg"}}
+      />
     </View>
   );
 }
@@ -13,8 +47,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'orange',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    width: 200,
+    borderWidth: 1,
+    borderColor: 'orange',
+  },
+  image: {
+    width: 250,
+    height: 150,
+  }
 });
